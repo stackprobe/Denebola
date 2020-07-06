c105742d087486511a331137a44f4ffa99d71c85
stackprobe <stackprobes@gmail.com>
2020-07-06 21:16:28 +0900

    Beauty

---
+++ 20200703_TestDLL/t0001/DLLTest0001.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace t0001
+{
+	public class DLLTest0001
+	{
+		public string Echo(string message)
+		{
+			return message;
+		}
+	}
+}
